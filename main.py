3165f42868508a92b222111d848eb943d9f875ca
BaH96p0 <144206430+BaH96p0@users.noreply.github.com>
2023-10-18 21:52:20 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+n = random.random()
+k = random.randint(10,15)
+
+rr = random.randrange(10)
+rrs = random.randrange(33, 88)
+rrss = random.randrange(18, 50, 6)
+
+s = [1, 2, 3, 4, 5, 6, 7]
+random.shuffle(s)
+n = random.choice(s)
+
+
